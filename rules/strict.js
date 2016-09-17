736cca37fdc1cee6e7a7a85e39477a4eb5f86784
Nicolas Jakob <nicow.jakob@gmail.com>
2016-09-17 10:49:57 +0200

    Fix styling in rules

--- rules/strict.js
+++ rules/strict.js
@@ -3,6 +3,6 @@ module.exports = {
   rules: {
     // Require or disallow strict mode directives
     // Since babel inserts `'use strict';` for us, we don't need it
-    strict: [ 'error', 'never' ]
+    strict: ['error', 'never']
   }
 };
